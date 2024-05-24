6a3dd5b1db1aaffe4da5031a928a422c5e9dce3c
RubiaLuque <muxu@ucm.es>
2024-05-24 14:30:19 +0200

    entorno nivel 1 + movimiento mal

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour
 {
-    CharacterController controller
-        ;
+    CharacterController controller;
+    Animator animator;
+    Transform transform;
+
     [SerializeField]
     float speed = 5f;
 
@@ -13,12 +15,37 @@ public class CharacterMove : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(speed * move * Time.deltaTime);
+        Vector3 move;
+        animator.Play("Idle");
+    
+        if (Input.GetKey(KeyCode.W))
+        {
+            move = transform.forward;
+            transform.Translate(speed * move * Time.deltaTime);
+            animator.Play("RunForward");
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            move = transform.forward * (-1);
+            transform.Translate(move * Time.deltaTime * speed);
+            animator.Play("RunBackward");
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.RotateAround(new Vector3(0, 1, 0), -1);
+            animator.Play("RunForward");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.RotateAround(new Vector3(0, 1, 0), 1);
+            animator.Play("RunForward");
+        }
     }
 }

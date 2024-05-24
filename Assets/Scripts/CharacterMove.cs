8a6936791f1ea59a08a459bce75615a88b704c1c
RubiaLuque <muxu@ucm.es>
2024-05-24 18:33:57 +0200

    movimiento y animaciones player

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -5,47 +5,66 @@ using UnityEngine;
 public class CharacterMove : MonoBehaviour
 {
     CharacterController controller;
-    Animator animator;
-    Transform transform;
+    public Animator animator;
 
     [SerializeField]
     float speed = 5f;
+
+    [SerializeField]
+    float rotationSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
-        animator = GetComponent<Animator>();
-        transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 move;
-        animator.Play("Idle");
     
         if (Input.GetKey(KeyCode.W))
         {
             move = transform.forward;
-            transform.Translate(speed * move * Time.deltaTime);
-            animator.Play("RunForward");
+            controller.Move(speed * move.normalized * Time.deltaTime);
+            //transform.position = this.transform.position + speed * move.normalized * Time.deltaTime;
+            animator.SetBool("runForward", true);
         }
         else if (Input.GetKey(KeyCode.S))
         {
             move = transform.forward * (-1);
-            transform.Translate(move * Time.deltaTime * speed);
-            animator.Play("RunBackward");
+            controller.Move(speed * move.normalized * Time.deltaTime);
+            animator.SetBool("runBackward", true);
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            transform.RotateAround(new Vector3(0, 1, 0), -1);
-            animator.Play("RunForward");
+            transform.Rotate(new Vector3(0, 1, 0), -1 * Time.deltaTime * rotationSpeed);
+            animator.SetBool("runForward", true);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.RotateAround(new Vector3(0, 1, 0), 1);
-            animator.Play("RunForward");
+            transform.Rotate(new Vector3(0, 1, 0), Time.deltaTime* rotationSpeed);
+            animator.SetBool("runForward", true);
         }
+        else if(Input.GetKeyDown(KeyCode.E))
+        {
+            animator.SetTrigger("attacking");
+            animator.SetBool("runForward", false);
+            animator.SetBool("runBackward", false);
+        }
+        else
+        {
+            animator.SetBool("runForward", false);
+            animator.SetBool("runBackward", false);
+            animator.SetBool("dead", false); //Quitar esta linea despues
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * 4);
     }
 }

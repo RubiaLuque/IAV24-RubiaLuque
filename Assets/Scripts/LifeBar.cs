6d5a2f366b03ec502747d95e614a9a13c29cb5f8
RubiaLuque <muxu@ucm.es>
2024-05-26 22:22:13 +0200

    eliminacion assets no utilizados

--- Assets/Scripts/LifeBar.cs
+++ Assets/Scripts/LifeBar.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class LifeBar : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +13,11 @@ public class LifeBar : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    public void ResizeBar(int damage)
     {
         
     }
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public LifeBar lifeBar;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +17,6 @@ public class PlayerHealth : MonoBehaviour
     {
         
     }
+
+
 }

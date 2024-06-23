2874c11232ebdca2d965c8d4d8d2a85288111f7c
RubiaLuque <muxu@ucm.es>
2024-06-23 21:49:48 +0200

    arreglos para build

--- Assets/Scripts/IA/FSM/Actions/Wander.cs
+++ Assets/Scripts/IA/FSM/Actions/Wander.cs
@@ -1,7 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.AI;
 

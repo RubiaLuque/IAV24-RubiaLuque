255bce933c33d9352097738be0a0b86c48911edc
RubiaLuque <muxu@ucm.es>
2024-06-23 18:30:12 +0200

    mas cambios nivel 1 y assets

--- Assets/Scripts/IA/FSM/FSMDef.cs
+++ Assets/Scripts/IA/FSM/FSMDef.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 public class FSMDef : ScriptableObject
 {
     public string initialState;
-    public List<string> idActions = new List<string>(); //Aquí guardas nombres para los estados (p.ej Wander1, Wander2, Sigue, etc)
-    public List<string> actions = new List<string>(); //Aquí metes los nombres de las acciones que se tienen en cuenta en MachineManager
+    public List<string> idActions = new List<string>(); //Se guardan los estados (WanderState, FollowState, etc)
+    public List<string> actions = new List<string>(); //Nombres de las acciones que se tienen en cuenta en MachineManager
     //(WanderAction, FollowAction o HideAction) de modo que idActions[i] es una accion de tipo actions[i]
 
 
     public List<string> decisions = new List<string>(); //Lo mismo que actions pero para transiciones
-    public List<string> nextActions = new List<string>(); //id de la accion a la que va idTransicions[i]
-    public List<string> previousActions = new List<string>(); //id de la accion desde la que viene Transicions[i]
+    public List<string> nextActions = new List<string>(); //id de la accion a la que va decisions[i]
+    public List<string> previousActions = new List<string>(); //id de la accion desde la que viene decisions[i]
 
 }
--- Assets/Scripts/IA/FSM/MachineManager.cs
+++ Assets/Scripts/IA/FSM/MachineManager.cs
@@ -8,8 +8,6 @@ using UnityEngine;
 
 public class MachineManager : MonoBehaviour
 {
-    //public FSMDef definition;
-
     private void Awake()
     {
         
